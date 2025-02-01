f85d65078fa0a4da7f811ca7f9d54410cf0419f7
DEV BHUTANI <devbhutani10@gmail.com>
2025-02-01 20:55:56 +0530

    login page and backend connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import UseCard from './components/UseCard'
-
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import LandingPage from './pages/LandingPage'
+import { Toaster } from './components/ui/toaster'
+import Dashboard from './pages/Dashboard'
 function App() {
 
 
   return (
     <>
-    <div className='flex justify-center items-center h-screen w-screen'>
-    <UseCard/>
-    </div>
-     
+    <Routes>
+      <Route path='/' element={<LandingPage/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+    </Routes>
+    <Toaster/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter as Router} from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios'
+
+
+const apiUrl =import.meta.env.VITE_API_URL
+axios.defaults.baseURL = apiUrl
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
 )
